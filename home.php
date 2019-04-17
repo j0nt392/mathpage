980a58543ebe5994310a68c88dc61ac7d43a352c
j0nt392 <jonte.arnberg@gmail.com>
2019-04-17 12:37:38 +0200

    second commit

---
+++ home.php
@@ -0,0 +1,138 @@
+<?php
+// We need to use sessions, so you should always start sessions using the below code.
+session_start();
+// If the user is not logged in redirect to the login page...
+if (!isset($_SESSION['loggedin'])) {
+	header('Location: index.html');
+	exit();
+}
+?>
+
+<!DOCTYPE html>
+<html>
+	<head>
+		<meta charset="utf-8">
+		<title>Home Page</title>
+		<link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jodit/3.1.39/jodit.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jodit/3.1.39/jodit.min.js"></script>
+
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
+	</head>
+	<body class="loggedin">
+		<nav class="navtop">
+			<div>
+				<h1>Math Zone</h1>
+				<a href="profile.php"><i class="fas fa-user-circle"></i>Profile</a>
+				<a href="logout.php"><i class="fas fa-sign-out-alt"></i>Logout</a>
+			</div>
+		</nav>
+		<div class="content">
+			<h2>Home Page</h2>
+			<p>Welcome back, <?=$_SESSION['name']?>!</p>
+
+      <?php
+
+      //connects to the database
+      $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+          if (mysqli_connect_errno())
+          {
+          echo "Något gick fel: " . mysql_connect_error();
+          }
+
+      //gets table and row
+			$ID = $_SESSION['id'];
+      $SQL =  "SELECT texts FROM sections WHERE id = '$_SESSION[id]'"
+			         ;
+
+			echo '<div class="my_class" style="height:200px; overflow-y: scroll;">';
+      //conditions the upload
+      $resultat = mysqli_query($mysql_pointer, $SQL);
+      while($rad = mysqli_fetch_array($resultat))
+            {
+            echo  $_SESSION['id'] . " " . $rad['texts'];
+            }
+						echo '</div>';
+
+            if (isset($_GET['garfieldsbutton'])) {
+              $query="INSERT INTO sections(texts, id)
+                      VALUES('$_GET[garfieldstextarea]', $ID)";
+
+                        if(!mysqli_query($mysql_pointer,$query))
+                        {
+                          die('Error . ' . mysqli_error($mysql_pointer));
+                        }
+                        header("Location: home.php");
+                      }
+
+                      /* If upload button is clicked ...
+                       if (isset($_POST['upload'])) {
+                       	// Get image name
+                       	$image = $_FILES['image']['name'];
+                       	// Get text
+                       	$image_text = mysqli_real_escape_string($mysql_pointer, $_POST['image_text']);
+
+                       	// image file directory
+                       	$target = "images/".basename($image);
+
+                       	$sql = "INSERT INTO image_table (image, imagetext) VALUES ('$image', '$image_text')";
+                       	// execute query
+                       	mysqli_query($mysql_pointer, $sql);
+
+                       	if (move_uploaded_file($_FILES['image']['tmp_name'], $target)) {
+
+                       		$msg = "Image uploaded successfully";
+                          header("Location: index.php");
+                          exit;
+                       	}else{
+                       		$msg = "Failed to upload image";
+                       	}
+                       }
+                       $result = mysqli_query($mysql_pointer, "SELECT * FROM image_table");*/
+                     ?>
+
+
+
+
+
+          <!-- this section uploads TEXT -->
+          <div id="inputs">
+					<p>This is a HTML editor, not a text editor. But you can use it as a text editor.
+						**to make a linebreak, hold shift and press enter'</p>
+          <form id="inputs" action="home.php" method="GET">
+          <textarea type="text" name="garfieldstextarea" id="garfieldstextarea" style ="width : 200px; height : 200px">
+          </textarea>
+          <br>
+
+
+          <script>
+          var editor = new Jodit('#garfieldstextarea');
+          </script>
+
+          <input  name="garfieldsbutton" type="submit" value="UPLOAD YOUR SHIT Garfield "></input>
+          </form>
+
+
+
+          <!-- this section uploads FILES such as image
+          <form id="images" method="POST" action="home.php" enctype="multipart/form-data">
+          	<input type="hidden" name="size" value="1000000">
+          	<div>
+          	  <input type="file" name="image">
+          	</div>
+          	<div>
+              <textarea
+              	id="text"
+              	cols="40"
+              	rows="4"
+              	name="image_text"
+              	placeholder="Say something about this image..."></textarea>
+          	</div>
+          	<div>
+          		<button type="submit" name="upload">POST</button>
+          	</div>
+          </form> -->
+        </div>
+		</div>
+	</body>
+</html>
--- homepage.php
+++ homepage.php
@@ -3,47 +3,324 @@
   <head>
     <meta charset="utf-8">
     <title></title>
+    <script src="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/js/bootstrap.min.js" integrity="sha384-JjSmVgyd0p3pXB1rRibZUAYoIIy6OrQ6VrjIEaFf/nJGzIxFDsf4x0xIM+B07jRM" crossorigin="anonymous"></script>
+    <link href="https://stackpath.bootstrapcdn.com/bootstrap/4.3.1/css/bootstrap.min.css" rel="stylesheet" integrity="sha384-ggOyR0iXCbMQv3Xipma34MD+dH/1fQ784/j6cY/iJTQUOhcWr7x9JvoRxT2MZw1T" crossorigin="anonymous">
+    <link href="style.css" rel="stylesheet" type="text/css">
+    <link rel="stylesheet" href="//cdnjs.cloudflare.com/ajax/libs/jodit/3.1.39/jodit.min.css">
+    <script src="//cdnjs.cloudflare.com/ajax/libs/jodit/3.1.39/jodit.min.js"></script>
+		<link rel="stylesheet" href="https://use.fontawesome.com/releases/v5.7.1/css/all.css">
   </head>
 <style>
 
 
 #maths {
-  border-style:solid;
-  border-color:#287EC7;
-  height: 400px;
-  width: 300px;
-  background-color: white;
+
+  height: 950px;
+  width: 1900px;
+  right: 2px;
   position: absolute;
   top: 300px;
-  left: 200px;
+  left: 50px;
+
 }
 
+.paragraph-math {
+  background-color: white;
 
-header
-{
-  background-color: red;
-  height: 100px;
-  text-align: center;
-  font-size: 90px;
+
+  height: 400px;
+  width: 400px;
+
+}
+
+.paragraph-pretender {
+  background-color: white;
+
+
+  height: 400px;
+  width: 400px;
+  left: 450px;
+  position: relative;
+}
+
+.paragraph-garfield {
+  background-color: white;
+
+
+  height: 400px;
+  width: 400px;
+  left: 900px;
+  position: relative;
+}
+
+.paragraph-euroFag {
+  background-color: white;
+
+
+  height: 400px;
+  width: 400px;
+  left: 1350px;
+  position: relative;
+}
+
+.paragraph-fluegs{
+  background-color: white;
+  height: 400px;
+  width: 400px;
+  top: 500px;
+  position: relative;
+}
+
+.paragraph-deceiver {
+  background-color: white;
+
+
+  height: 400px;
+  width: 400px;
+  left: 450px;
+  top: 500px;
+  position: relative;
+}
+
+.paragraph-alpha {
+  background-color: white;
+
+
+  height: 400px;
+  width: 400px;
+  left: 900px;
+  top: 500px;
+  position: relative;
+}
+
+.paragraph-highPitch {
+  background-color: white;
+  height: 400px;
+  width: 400px;
+  left: 1350px;
+  top: 500px;
+  position: relative;
+}
+
+.heading{
+  position: absolute;
+  color: white;
+  font-family: myFirstFonts;
+  left:680px;
+  top: 50px;
+  font-size: 60px;
+}
+
+.intro{
+  position: absolute;
+  right: 500px;
+  top: 280px;
+  color: white;
+}
+
+.mathname{
+  color: white;
+  position: absolute;
+  left: 50px;
+  top: 200px;
+}
+
+.pretendername{
+color: white;
+position: absolute;
+left: 500px;
+top: 200px;
+}
+
+.garfieldname{
+color: white;
+position: absolute;
+left: 950px;
+top: 200px;
+}
+
+.eurofagname{
+color: white;
+position: absolute;
+left: 1400px;
+top: 200px;
+}
+
+.fluegsname{
+color: white;
+position: absolute;
+top: 720px;
+left: 50px;
+}
+
+.deceivername{
+color: white;
+position: absolute;
+top: 720px;
+left: 500px;
+}
+
+.blackkidname{
+color: white;
+position: absolute;
+top: 720px;
+left: 950px;
+}
+
+.highpitchname{
+color: white;
+position: absolute;
+top: 720px;
+left: 1400px;
 }
 
 body
 {
-  background-color: blue;
+  background-image: url(images/Untitled-1.jpg);
+  background-repeat: no-repeat;
+  background-size: cover;
 }
+@font-face {
+   font-family: myFirstFonts;
+   src: url('images/SketchesByDuerer.ttf') format('truetype');
+}
+@media only screen and (max-width: 768px) {
+  /* For mobile phones: */
 
+  body
+  {
+    background-image: url(images/Untitled-1.jpg);
+    background-repeat: repeat-y;
+    background-size: cover;
+  }
+
+  [class*="heading"] {
+    left:50px;
+    top: 50px;
+  }
+
+  [class*="paragraph-math"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 0px;
+  }
+
+  [class*="mathname"] {
+    top: 230px;
+  }
+  [class*="paragraph-pretender"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 480px;
+    left: 0px;
+  }
+  [class*="pretendername"] {
+    top: 720px;
+    left: 50px;
+  }
+  [class*="paragraph-garfield"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 960px;
+    left: 0px;
+  }
+  [class*="garfieldname"] {
+    top: 1210px;
+    left: 50px;
+  }
+  [class*="paragraph-euroFag"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 1440px;
+    left: 0px;
+  }
+
+  [class*="eurofagname"] {
+    top: 1700px;
+    left: 50px;
+  }
+  [class*="paragraph-fluegs"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 1920px;
+    left: 0px;
+  }
+  [class*="fluegsname"] {
+    top: 2150px;
+    left: 50px;
+  }
+  [class*="paragraph-deceiver"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 2400px;
+    left: 0px;
+  }
+  [class*="deceivername"] {
+    top: 2630px;
+    left: 50px;
+  }
+  [class*="paragraph-alpha"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 2880px;
+    left: 0px;
+  }
+  [class*="blackkidname"] {
+    top: 3110px;
+    left: 50px;
+  }
+  [class*="paragraph-highPitch"] {
+    width: 100%;
+    background-color: white;
+    height: 400px;
+    width: 400px;
+    top: 3360px;
+    left: 0px;
+  }
+  [class*="highpitchname"] {
+    top: 3590px;
+    left: 50px;
+  }
+}
 
 </style>
 
 
 <body>
 
-<header>MATH NEWS</header>
+<!--<div class="intro">
+  <h1>Lorem ipsum</h1>
+  <p>"Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut<br> labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud <br>exercitation ullamco laboris nisi ut <br>aliquip ex ea commodo consequat. Duis aute irure dolor<br> in reprehenderit in voluptate velit esse <br>cillum dolore eu fugiat nulla pariatur. Excepteur <br>sint occaecat cupidatat non <br>proident, sunt in culpa qui officia deserunt <br>mollit anim id est laborum."</p><br>
+  <p>"Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut<br> labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud <br>exercitation ullamco laboris nisi ut <br>aliquip ex ea commodo consequat. Duis aute irure dolor<br> in reprehenderit in voluptate velit esse <br>cillum dolore eu fugiat nulla pariatur. Excepteur <br>sint occaecat cupidatat non <br>proident, sunt in culpa qui officia deserunt <br>mollit anim id est laborum."</p>
+</div> -->
 
+<h1 class="heading">Math ZONE</h1>
+    <h1 class="mathname" > sixxside</h1>
+    <h1 class="pretendername"> cube </h1>
+    <h1 class="garfieldname"> Garfield </h1>
+    <h1 class="eurofagname"> Eurofag </h1>
+    <h1 class="fluegsname"> Fluegs</h1>
+    <h1 class="deceivername"> Deceiver </h1>
+    <h1 class="blackkidname"> Blackkid </h1>
+    <h1 class="highpitchname"> HighPitch </h1>
 
+<div id="maths" style= "position: absolute;">
 
-    <h1 style="position: absolute; left: 200px; top: 200px;">Maths Page </h1>
-    <div id="maths">
+<div class="paragraph-math" style= "overflow-y: scroll; position: absolute;">
 
         <?php
         $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
@@ -52,16 +329,193 @@ body
             echo "Något gick fel: " . mysql_connect_error();
             }
 
-        $SQL =  'SELECT * FROM sections';
 
-        $resultat = mysqli_query($mysql_pointer, $SQL);
 
-        while($rad = mysqli_fetch_array($resultat))
-              {
-                echo $rad['text'];
-                echo "<br>";
-              }
+            $SQL =  "SELECT texts FROM sections WHERE id = 1"
+      			         ;
+
+
+            //conditions the upload
+            $resultat = mysqli_query($mysql_pointer, $SQL);
+            while($rad = mysqli_fetch_array($resultat))
+                  {
+                  echo   $rad['texts'];
+                  }
+
+
         ?>
-        </div>
+</div>
+
+<div class="paragraph-pretender" style= "overflow-y: scroll; position: absolute;">
+
+    <?php
+    $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+        if (mysqli_connect_errno())
+        {
+        echo "Något gick fel: " . mysql_connect_error();
+        }
+
+    $SQL =  'SELECT * FROM sections
+              WHERE id = 15 ';
+
+    $resultat = mysqli_query($mysql_pointer, $SQL);
+
+    while($rad = mysqli_fetch_array($resultat))
+          {
+            echo 'Hej ';
+            echo $rad['id'] . " " . $rad['texts'] . " ";
+
+          }
+    ?>
+  </div>
+
+<div class="paragraph-garfield" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 16 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+<div class="paragraph-euroFag" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 17 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+<div class="paragraph-fluegs" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 18 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+<div class="paragraph-deceiver" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 19 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+<div class="paragraph-alpha" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 20 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+<div class="paragraph-highPitch" style= "overflow-y: scroll; position: absolute;">
+
+  <?php
+  $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+      if (mysqli_connect_errno())
+      {
+      echo "Något gick fel: " . mysql_connect_error();
+      }
+
+  $SQL =  'SELECT * FROM sections
+            WHERE id = 21 ';
+
+  $resultat = mysqli_query($mysql_pointer, $SQL);
+
+  while($rad = mysqli_fetch_array($resultat))
+        {
+          echo 'Hej ';
+          echo $rad['id'] . " " . $rad['texts'] . " ";
+
+        }
+  ?>
+
+</div>
+
+</div>
+
+
+
   </body>
 </html>
