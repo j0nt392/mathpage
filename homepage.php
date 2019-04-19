9d67f7c920efdceda96cd4af7309d559d24fe5b5
j0nt392 <jonte.arnberg@gmail.com>
2019-04-20 00:03:59 +0200

    fourth commit

--- homepage.php
+++ homepage.php
@@ -1,5 +1,7 @@
 <!DOCTYPE html>
 <html lang="en" dir="ltr">
+<meta name="viewport" content="width=device-width, initial-scale=1.0">
+
   <head>
     <meta charset="utf-8">
     <title></title>
@@ -13,306 +15,241 @@
 <style>
 
 
-#maths {
+  #maths {
+    height: 950px;
+    width: 1900px;
+    right: 2px;
+    position: absolute;
+    top: 300px;
+    left: 50px;
+  }
 
-  height: 950px;
-  width: 1900px;
-  right: 2px;
-  position: absolute;
-  top: 300px;
-  left: 50px;
+  .paragraph-math, .paragraph-pretender, .paragraph-garfield,
+  .paragraph-euroFag, .paragraph-fluegs, .paragraph-deceiver,
+  .paragraph-alpha, .paragraph-highPitch {
+    background-color: white;
+    height:400px;
+    width:400px;
+    position: relative;
+  }
 
-}
+  .paragraph-math {
+    left: 0px;
+  }
 
-.paragraph-math {
-  background-color: white;
+  .paragraph-pretender
+  {
+    left: 450px;
+  }
 
+  .paragraph-garfield {
+    left: 900px;
+  }
 
-  height: 400px;
-  width: 400px;
+  .paragraph-euroFag {
+    left: 1350px;
+  }
 
-}
+  .paragraph-fluegs{
+    top: 500px;
+  }
 
-.paragraph-pretender {
-  background-color: white;
+  .paragraph-deceiver {
+    left: 450px;
+    top: 500px;
+  }
 
+  .paragraph-alpha {
+    left: 900px;
+    top: 500px;
+  }
 
-  height: 400px;
-  width: 400px;
-  left: 450px;
-  position: relative;
-}
+  .paragraph-highPitch {
+    left: 1350px;
+    top: 500px;
+  }
 
-.paragraph-garfield {
-  background-color: white;
+  .heading{
+    position: absolute;
+    font-family: myFirstFonts;
+    color: white;
+    left:680px;
+    top: 50px;
+  }
 
+  .intro{
+    position: absolute;
+    right: 500px;
+    top: 280px;
+    color: white;
+  }
 
-  height: 400px;
-  width: 400px;
-  left: 900px;
-  position: relative;
-}
+  .mathname, .pretendername, .garfieldname, .eurofagname,.fluegsname,
+   .deceivername, .blackkidname, .highpitchname{
+    color: white;
+    position: absolute;
+  }
 
-.paragraph-euroFag {
-  background-color: white;
+  .mathname{
+    left: 50px;
+    top: 200px;
+  }
 
-
-  height: 400px;
-  width: 400px;
-  left: 1350px;
-  position: relative;
-}
-
-.paragraph-fluegs{
-  background-color: white;
-  height: 400px;
-  width: 400px;
-  top: 500px;
-  position: relative;
-}
-
-.paragraph-deceiver {
-  background-color: white;
-
-
-  height: 400px;
-  width: 400px;
-  left: 450px;
-  top: 500px;
-  position: relative;
-}
-
-.paragraph-alpha {
-  background-color: white;
-
-
-  height: 400px;
-  width: 400px;
-  left: 900px;
-  top: 500px;
-  position: relative;
-}
-
-.paragraph-highPitch {
-  background-color: white;
-  height: 400px;
-  width: 400px;
-  left: 1350px;
-  top: 500px;
-  position: relative;
-}
-
-.heading{
-  position: absolute;
-  color: white;
-  font-family: myFirstFonts;
-  left:680px;
-  top: 50px;
-  font-size: 60px;
-}
-
-.intro{
-  position: absolute;
-  right: 500px;
-  top: 280px;
-  color: white;
-}
-
-.mathname{
-  color: white;
-  position: absolute;
-  left: 50px;
+  .pretendername{
+  left: 500px;
   top: 200px;
-}
+  }
 
-.pretendername{
-color: white;
-position: absolute;
-left: 500px;
-top: 200px;
-}
+  .garfieldname{
+  left: 950px;
+  top: 200px;
+  }
 
-.garfieldname{
-color: white;
-position: absolute;
-left: 950px;
-top: 200px;
-}
+  .eurofagname{
+  left: 1400px;
+  top: 200px;
+  }
 
-.eurofagname{
-color: white;
-position: absolute;
-left: 1400px;
-top: 200px;
-}
+  .fluegsname{
+  top: 720px;
+  left: 50px;
+  }
 
-.fluegsname{
-color: white;
-position: absolute;
-top: 720px;
-left: 50px;
-}
+  .deceivername{
+  top: 720px;
+  left: 500px;
+  }
 
-.deceivername{
-color: white;
-position: absolute;
-top: 720px;
-left: 500px;
-}
+  .blackkidname{
 
-.blackkidname{
-color: white;
-position: absolute;
-top: 720px;
-left: 950px;
-}
+  top: 720px;
+  left: 950px;
+  }
 
-.highpitchname{
-color: white;
-position: absolute;
-top: 720px;
-left: 1400px;
-}
+  .highpitchname{
 
-body
-{
-  background-image: url(images/Untitled-1.jpg);
-
-}
-@font-face {
-   font-family: myFirstFonts;
-   src: url('images/SketchesByDuerer.ttf') format('truetype');
-}
-@media only screen and (max-width: 768px) {
-  /* For mobile phones: */
+  top: 720px;
+  left: 1400px;
+  }
 
   body
   {
     background-image: url(images/Untitled-1.jpg);
-    background-repeat: repeat-y;
-    background-size: cover;
-  }
 
-  [class*="heading"] {
-    left:50px;
-    top: 50px;
   }
+  @font-face {
+     font-family: myFirstFonts;
+     src: url('images/SketchesByDuerer.ttf') format('truetype');
+  }
+  @media only screen and (max-width: 768px) {
+    /* For mobile phones: */
 
-  [class*="paragraph-math"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 0px;
-  }
+    body
+    {
+      background-image: url(images/Untitled-1.jpg);
+      background-repeat: repeat-y;
+      background-size: cover;
+    }
 
-  [class*="mathname"] {
-    top: 230px;
-  }
-  [class*="paragraph-pretender"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 480px;
-    left: 0px;
-  }
-  [class*="pretendername"] {
-    top: 720px;
-    left: 50px;
-  }
-  [class*="paragraph-garfield"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 960px;
-    left: 0px;
-  }
-  [class*="garfieldname"] {
-    top: 1210px;
-    left: 50px;
-  }
-  [class*="paragraph-euroFag"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 1440px;
-    left: 0px;
-  }
+    [class*="heading"] {
+      left:50px;
+      top: 50px;
+    }
 
-  [class*="eurofagname"] {
-    top: 1700px;
-    left: 50px;
+    [class*="paragraph-math"] {
+      top: 0px;
+    }
+
+    [class*="mathname"] {
+      top: 230px;
+    }
+    [class*="paragraph-pretender"] {
+      top: 480px;
+      left: 0px;
+    }
+    [class*="pretendername"] {
+      top: 720px;
+      left: 50px;
+    }
+    [class*="paragraph-garfield"] {
+      top: 960px;
+      left: 0px;
+    }
+    [class*="garfieldname"] {
+      top: 1210px;
+      left: 50px;
+    }
+    [class*="paragraph-euroFag"] {
+      top: 1440px;
+      left: 0px;
+    }
+
+    [class*="eurofagname"] {
+      top: 1700px;
+      left: 50px;
+    }
+    [class*="paragraph-fluegs"] {
+      top: 1920px;
+      left: 0px;
+    }
+    [class*="fluegsname"] {
+      top: 2150px;
+      left: 50px;
+    }
+    [class*="paragraph-deceiver"] {
+      top: 2400px;
+      left: 0px;
+    }
+    [class*="deceivername"] {
+      top: 2630px;
+      left: 50px;
+    }
+    [class*="paragraph-alpha"] {
+      top: 2880px;
+      left: 0px;
+    }
+    [class*="blackkidname"] {
+      top: 3110px;
+      left: 50px;
+    }
+    [class*="paragraph-highPitch"] {
+      top: 3360px;
+      left: 0px;
+    }
+    [class*="highpitchname"] {
+      top: 3590px;
+      left: 50px;
+    }
   }
-  [class*="paragraph-fluegs"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 1920px;
-    left: 0px;
-  }
-  [class*="fluegsname"] {
-    top: 2150px;
-    left: 50px;
-  }
-  [class*="paragraph-deceiver"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 2400px;
-    left: 0px;
-  }
-  [class*="deceivername"] {
-    top: 2630px;
-    left: 50px;
-  }
-  [class*="paragraph-alpha"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 2880px;
-    left: 0px;
-  }
-  [class*="blackkidname"] {
-    top: 3110px;
-    left: 50px;
-  }
-  [class*="paragraph-highPitch"] {
-    width: 100%;
-    background-color: white;
-    height: 400px;
-    width: 400px;
-    top: 3360px;
-    left: 0px;
-  }
-  [class*="highpitchname"] {
-    top: 3590px;
-    left: 50px;
-  }
-}
 
 </style>
 
-
 <body>
-
-<!--<div class="intro">
-  <h1>Lorem ipsum</h1>
-  <p>"Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut<br> labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud <br>exercitation ullamco laboris nisi ut <br>aliquip ex ea commodo consequat. Duis aute irure dolor<br> in reprehenderit in voluptate velit esse <br>cillum dolore eu fugiat nulla pariatur. Excepteur <br>sint occaecat cupidatat non <br>proident, sunt in culpa qui officia deserunt <br>mollit anim id est laborum."</p><br>
-  <p>"Lorem ipsum dolor sit amet,<br> consectetur adipiscing elit, <br> sed do eiusmod tempor incididunt ut<br> labore et dolore magna aliqua.<br> Ut enim ad minim veniam, quis nostrud <br>exercitation ullamco laboris nisi ut <br>aliquip ex ea commodo consequat. Duis aute irure dolor<br> in reprehenderit in voluptate velit esse <br>cillum dolore eu fugiat nulla pariatur. Excepteur <br>sint occaecat cupidatat non <br>proident, sunt in culpa qui officia deserunt <br>mollit anim id est laborum."</p>
-</div> -->
 <?php
-$mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
-    if (mysqli_connect_errno())
-    {
-    echo "Något gick fel: " . mysql_connect_error();
-    }  ?>
+//SQL function to figure out what row to fetch and post
+        function writeMessage($idnum) {
 
+          $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
+              if (mysqli_connect_errno())
+              {
+              echo "Något gick fel: " . mysql_connect_error();
+              }
+
+          $SQL =  'SELECT * FROM sections
+                    WHERE id = '.$idnum.' ';
+
+          $resultat = mysqli_query($mysql_pointer, $SQL);
+
+          while($rad = mysqli_fetch_array($resultat))
+                {
+                  //two first ones are for testing purposes
+                  //echo 'Hej ';
+                  //echo $rad['id'];
+                  echo $rad['texts'];
+                }
+} ?>
+
+<!-- logotype -->
 <h1 class="heading">Math ZONE</h1>
     <h1 class="mathname" > sixxside</h1>
     <h1 class="pretendername"> cube </h1>
@@ -323,156 +260,75 @@ $mysql_pointer = mysqli_connect("localhost", "root", "", "mathpage");
     <h1 class="blackkidname"> Blackkid </h1>
     <h1 class="highpitchname"> HighPitch </h1>
 
+<!--container for all the columns-->
 <div id="maths" style= "position: absolute;">
 
-<div class="paragraph-math" style= "overflow-y: scroll; position: absolute;">
 
-  <?php
-        $SQLMATH =  "SELECT texts FROM sections WHERE id = 1";
-        //conditions the upload
-        $resultat = mysqli_query($mysql_pointer, $SQLMATH);
-        while($rad = mysqli_fetch_array($resultat))
-              {
-              echo   $rad['texts'];
-              }
-    ?>
+      <div class="paragraph-math" style= "overflow-y: scroll; position: absolute;">
 
+            <?php
+              /* Calling a PHP Function */
+              writeMessage('1');
+              ?>
 
-</div>
+      </div>
 
-<div class="paragraph-pretender" style= "overflow-y: scroll; position: absolute;">
+      <div class="paragraph-pretender" style= "overflow-y: scroll; position: absolute;">
 
-    <?php
-    $SQLPRETENDER =  'SELECT * FROM sections
-              WHERE id = 15 ';
+            <?php
+            writeMessage('15');
+            ?>
+        </div>
 
-    $resultat = mysqli_query($mysql_pointer, $SQLPRETENDER);
+      <div class="paragraph-garfield" style= "overflow-y: scroll; position: absolute;">
 
-    while($rad = mysqli_fetch_array($resultat))
-          {
-            echo 'Hej ';
-            echo $rad['id'] . " " . $rad['texts'] . " ";
+              <?php
+              writeMessage(16);
+              ?>
 
-          }
-    ?>
-  </div>
+      </div>
 
-<div class="paragraph-garfield" style= "overflow-y: scroll; position: absolute;">
+      <div class="paragraph-euroFag" style= "overflow-y: scroll; position: absolute;">
 
-  <?php
-  $SQLGARFIELD =  'SELECT * FROM sections
-            WHERE id = 16 ';
+              <?php
+              writeMessage(17);
+              ?>
 
-  $resultat = mysqli_query($mysql_pointer, $SQLGARFIELD);
+      </div>
 
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
+      <div class="paragraph-fluegs" style= "overflow-y: scroll; position: absolute;">
 
-        }
-  ?>
+              <?php
+              writeMessage(18);
+              ?>
 
-</div>
+      </div>
 
-<div class="paragraph-euroFag" style= "overflow-y: scroll; position: absolute;">
+      <div class="paragraph-deceiver" style= "overflow-y: scroll; position: absolute;">
 
-  <?php
+              <?php
+              writeMessage(19);
+              ?>
 
-  $SQLEUROFAG =  'SELECT * FROM sections
-            WHERE id = 17 ';
+      </div>
 
-  $resultat = mysqli_query($mysql_pointer, $SQLEUROFAG);
+      <div class="paragraph-alpha" style= "overflow-y: scroll; position: absolute;">
 
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
+              <?php
+              writeMessage(20);
+              ?>
 
-        }
-  ?>
+      </div>
+
+      <div class="paragraph-highPitch" style= "overflow-y: scroll; position: absolute;">
+
+              <?php
+              writeMessage(21);
+              ?>
+
+      </div>
 
 </div>
-
-<div class="paragraph-fluegs" style= "overflow-y: scroll; position: absolute;">
-
-  <?php
-
-  $SQLFLUEGS =  'SELECT * FROM sections
-            WHERE id = 18 ';
-
-  $resultat = mysqli_query($mysql_pointer, $SQLFLUEGS);
-
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
-
-        }
-  ?>
-
-</div>
-
-<div class="paragraph-deceiver" style= "overflow-y: scroll; position: absolute;">
-
-  <?php
-
-  $SQLDECEIVER =  'SELECT * FROM sections
-            WHERE id = 19 ';
-
-  $resultat = mysqli_query($mysql_pointer, $SQLDECEIVER);
-
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
-
-        }
-  ?>
-
-</div>
-
-<div class="paragraph-alpha" style= "overflow-y: scroll; position: absolute;">
-
-  <?php
-
-  $SQLALPHA =  'SELECT * FROM sections
-            WHERE id = 20 ';
-
-  $resultat = mysqli_query($mysql_pointer, $SQLALPHA);
-
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
-
-        }
-  ?>
-
-</div>
-
-<div class="paragraph-highPitch" style= "overflow-y: scroll; position: absolute;">
-
-  <?php
-
-  $SQLHIGHPITCH =  'SELECT * FROM sections
-            WHERE id = 21 ';
-
-  $resultat = mysqli_query($mysql_pointer, $SQLHIGHPITCH);
-
-  while($rad = mysqli_fetch_array($resultat))
-        {
-          echo 'Hej ';
-          echo $rad['id'] . " " . $rad['texts'] . " ";
-
-        }
-  ?>
-
-</div>
-
-</div>
-
-
 
   </body>
 </html>
